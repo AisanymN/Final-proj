c1fe7288e9c4ba5da04e7d5f4a009839e8fdd70a
Акбаян <200113015@stu.sdu.edu.kz>
2021-12-25 13:08:10 +0600

    Final

---
+++ js.js
@@ -0,0 +1,18 @@
+
+
+ 
+  var app = new Vue({
+    el: '#app',
+    data:{
+      isHidden:false
+    }
+
+  });
+
+  var apps = new Vue({
+    el: '#apps',
+    data:{
+      isHidden: false
+    }
+  });
+ 
